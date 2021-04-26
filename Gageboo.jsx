6a20caf073c276f1a06cf08169df08bb183e1cf0
hanseungbum <souq7635@naver.com>
2021-04-26 20:27:02 +0900

    삭제 로직 추가

--- Gageboo.jsx
+++ Gageboo.jsx
@@ -1,4 +1,5 @@
 
+const { max } = require('lodash');
 const React = require('react');
 const { useState, useEffect } = require('react');
 
@@ -9,29 +10,44 @@ const Gageboo = () => {
     const [money_show, setMoney_show] = useState('20,000,000');
     const [spend, setSpend] = useState('');
   const [result, setResult] = useState([]);
-  const [currentmoney, setCurrentmoney] = useState();
+  const [currentmoney, setCurrentmoney] = useState({});
   const [list, setList] = useState();
   const [time, setTime] = useState('');
   const [title, setTitle] = useState('');
-
+  const [list_index, setList_index]= useState(0);
   const inputEl = React.useRef(null);
-
+  
   const onSubmitForm = (e) => {
     e.preventDefault();
-      
+    setList_index(list_index+1);
+
     setMoney(money-spend);
-    
     
     inputEl.current.focus();
     
   };
 
-  useEffect(() => { // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
-    const List = [...result];
-    const Story = '날짜 : ' + time + ' 내역 : '+ title+ ' 금액 : '+ spend.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const onListdel = (e) => {
+      const mySpend =Number.parseInt(e.spend);
+      const myMoney =Number.parseInt(money)+mySpend;
+      
+    setResult(result.filter(x => { return x.id != e.id;}));
+    setMoney(myMoney);
+  
+    
+  };
+
+  useEffect((pre,next) => { // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
+    //const Story =  time + ' 내역 : '+ title+ ' 금액 : '+ spend.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const Story =  {id: list_index,
+                    time : time ,
+                    title : title,
+                    spend : spend //spend.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","
+                };
+
     setMoney_show(money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
     if(money!=='20000000'){
-         setResult([...List, Story]);
+         setResult([...result, Story]);
          setCurrentmoney(Story);
     }
 
@@ -42,7 +58,7 @@ const Gageboo = () => {
 
   useEffect(() => { // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
     
-    setList (result.map((i) => <li>{i}</li>));
+    setList (result.map((i,idx) => <li>{i.id+' '+i.spend} &nbsp; <button value={idx} onClick={()=>{onListdel(i)}} > 삭제</button></li>));//setResult([result.slice(0,i.idx),result.slice(i.idx+1,result.length+1)])
     return () => { // componentWillUnmount 역할
         
     }
@@ -79,7 +95,7 @@ const Gageboo = () => {
 
         
       </form>
-      <div>{currentmoney}</div>
+      <div>{currentmoney.id}</div>
       <div>[사용내역]</div>
       <div>{list}</div>
     </>
